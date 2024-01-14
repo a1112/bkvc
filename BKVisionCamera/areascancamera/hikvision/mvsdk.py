85cff84a9ba06da441789fa063f17d25654cac9a
LCX <1042815796@qq.com>
2024-01-14 12:58:25 +0800

    updata hik

--- BKVisionCamera/areascancamera/hikvision/mvsdk.py
+++ BKVisionCamera/areascancamera/hikvision/mvsdk.py
@@ -6,7 +6,7 @@ import numpy as np
 from BKVisionCamera.areascancamera.hikvision.MvImport.CameraParams_const import MV_GIGE_DEVICE, MV_USB_DEVICE, \
     MV_ACCESS_Exclusive, MV_CAMERALINK_DEVICE
 from BKVisionCamera.areascancamera.hikvision.MvImport.CameraParams_header import MV_CC_DEVICE_INFO_LIST, \
-    MV_CC_DEVICE_INFO, MV_TRIGGER_MODE_OFF, MV_FRAME_OUT_INFO_EX, MVCC_ENUMVALUE
+    MV_CC_DEVICE_INFO, MV_TRIGGER_MODE_OFF, MV_FRAME_OUT_INFO_EX, MVCC_ENUMVALUE, MVCC_INTVALUE
 from BKVisionCamera.areascancamera.hikvision.MvImport.MvCameraControl_class import MvCamera
 from base.property import CameraSdkInterface
 
@@ -39,6 +39,20 @@ class MvSdk(CameraSdkInterface):
             raise Exception("创建句柄失败")
         return stDevList, ret
 
+    def open(self, index=0):
+        self.init(index)
+        # 打开设备
+        self._open()
+        self.startGrabbing()
+
+    def _open(self):
+        # 打开设备
+        ret = self.cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
+        if ret != 0:
+            raise Exception("打开设备失败")
+        self.triggerMode = MV_TRIGGER_MODE_OFF
+        # 开始取流
+
     @property
     def triggerMode(self):
         stEnumValue = MVCC_ENUMVALUE()
@@ -47,18 +61,213 @@ class MvSdk(CameraSdkInterface):
             raise Exception("获取触发模式失败")
         return stEnumValue.nCurValue
 
-    def open(self):
-        # 打开设备
-        ret = self.cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
-        if ret != 0:
-            raise Exception("打开设备失败")
-        self.triggerMode = MV_TRIGGER_MODE_OFF
-        # 开始取流
-
-
     @triggerMode.setter
     def triggerMode(self, value=MV_TRIGGER_MODE_OFF):
         self.cam.MV_CC_SetEnumValue("TriggerMode", value)
+
+    @property
+    def exposureTime(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("ExposureTime", stParam)
+        if ret != 0:
+            raise Exception("获取曝光时间失败")
+        return stParam.nCurValue
+
+    @exposureTime.setter
+    def exposureTime(self, value):
+        self.cam.MV_CC_SetIntValue("ExposureTime", value)
+
+    @property
+    def gain(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("Gain", stParam)
+        if ret != 0:
+            raise Exception("获取增益失败")
+        return stParam.nCurValue
+
+    @gain.setter
+    def gain(self, value):
+        self.cam.MV_CC_SetIntValue("Gain", value)
+
+    @property
+    def gamma(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("Gamma", stParam)
+        if ret != 0:
+            raise Exception("获取Gamma失败")
+        return stParam.nCurValue
+
+    @gamma.setter
+    def gamma(self, value):
+        self.cam.MV_CC_SetIntValue("Gamma", value)
+
+    @property
+    def balanceRatioRed(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("BalanceRatioRed", stParam)
+        if ret != 0:
+            raise Exception("获取BalanceRatioRed失败")
+        return stParam.nCurValue
+
+    @balanceRatioRed.setter
+    def balanceRatioRed(self, value):
+        self.cam.MV_CC_SetIntValue("BalanceRatioRed", value)
+
+    @property
+    def balanceRatioGreen(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("BalanceRatioGreen", stParam)
+        if ret != 0:
+            raise Exception("获取BalanceRatioGreen失败")
+        return stParam.nCurValue
+
+    @balanceRatioGreen.setter
+    def balanceRatioGreen(self, value):
+        self.cam.MV_CC_SetIntValue("BalanceRatioGreen", value)
+
+    @property
+    def balanceRatioBlue(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("BalanceRatioBlue", stParam)
+        if ret != 0:
+            raise Exception("获取BalanceRatioBlue失败")
+        return stParam.nCurValue
+
+    @balanceRatioBlue.setter
+    def balanceRatioBlue(self, value):
+        self.cam.MV_CC_SetIntValue("BalanceRatioBlue", value)
+
+    @property
+    def balanceWhiteAuto(self):
+        stEnumValue = MVCC_ENUMVALUE()
+        ret = self.cam.MV_CC_GetEnumValue("BalanceWhiteAuto", stEnumValue)
+        if ret != 0:
+            raise Exception("获取BalanceWhiteAuto失败")
+        return stEnumValue.nCurValue
+
+    @balanceWhiteAuto.setter
+    def balanceWhiteAuto(self, value):
+        self.cam.MV_CC_SetEnumValue("BalanceWhiteAuto", value)
+
+    @property
+    def balanceRatioSelector(self):
+        stEnumValue = MVCC_ENUMVALUE()
+        ret = self.cam.MV_CC_GetEnumValue("BalanceRatioSelector", stEnumValue)
+        if ret != 0:
+            raise Exception("获取BalanceRatioSelector失败")
+        return stEnumValue.nCurValue
+
+    @balanceRatioSelector.setter
+    def balanceRatioSelector(self, value):
+        self.cam.MV_CC_SetEnumValue("BalanceRatioSelector", value)
+
+    @property
+    def balanceWhiteAutoOnce(self):
+        stEnumValue = MVCC_ENUMVALUE()
+        ret = self.cam.MV_CC_GetEnumValue("BalanceWhiteAutoOnce", stEnumValue)
+        if ret != 0:
+            raise Exception("获取BalanceWhiteAutoOnce失败")
+        return stEnumValue.nCurValue
+
+    @balanceWhiteAutoOnce.setter
+    def balanceWhiteAutoOnce(self, value):
+        self.cam.MV_CC_SetEnumValue("BalanceWhiteAutoOnce", value)
+
+    @property
+    def balanceRatio(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("BalanceRatio", stParam)
+        if ret != 0:
+            raise Exception("获取BalanceRatio失败")
+        return stParam.nCurValue
+
+    @balanceRatio.setter
+    def balanceRatio(self, value):
+        self.cam.MV_CC_SetIntValue("BalanceRatio", value)
+
+    @property
+    def payloadSize(self):
+        stCap = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("PayloadSize", stCap)
+        if ret != 0:
+            raise Exception("获取相机能力失败")
+        return stCap.nCurValue
+
+    @property
+    def width(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("Width", stParam)
+        if ret != 0:
+            raise Exception("获取图像宽度失败")
+        return stParam.nCurValue
+
+    @property
+    def height(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("Height", stParam)
+        if ret != 0:
+            raise Exception("获取图像高度失败")
+        return stParam.nCurValue
+
+    @property
+    def pixelFormat(self):
+        stEnumValue = MVCC_ENUMVALUE()
+        ret = self.cam.MV_CC_GetEnumValue("PixelFormat", stEnumValue)
+        if ret != 0:
+            raise Exception("获取像素格式失败")
+        return stEnumValue.nCurValue
+
+    @pixelFormat.setter
+    def pixelFormat(self, value):
+        self.cam.MV_CC_SetEnumValue("PixelFormat", value)
+
+    @property
+    def reverseX(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("ReverseX", stParam)
+        if ret != 0:
+            raise Exception("获取ReverseX失败")
+        return stParam.nCurValue
+
+    @reverseX.setter
+    def reverseX(self, value):
+        self.cam.MV_CC_SetIntValue("ReverseX", value)
+
+    @property
+    def reverseY(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("ReverseY", stParam)
+        if ret != 0:
+            raise Exception("获取ReverseY失败")
+        return stParam.nCurValue
+
+    @reverseY.setter
+    def reverseY(self, value):
+        self.cam.MV_CC_SetIntValue("ReverseY", value)
+
+    @property
+    def binningX(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("BinningX", stParam)
+        if ret != 0:
+            raise Exception("获取BinningX失败")
+        return stParam.nCurValue
+
+    @binningX.setter
+    def binningX(self, value):
+        self.cam.MV_CC_SetIntValue("BinningX", value)
+
+    @property
+    def binningY(self):
+        stParam = MVCC_INTVALUE()
+        ret = self.cam.MV_CC_GetIntValue("BinningY", stParam)
+        if ret != 0:
+            raise Exception("获取BinningY失败")
+        return stParam.nCurValue
+
+    @binningY.setter
+    def binningY(self, value):
+        self.cam.MV_CC_SetIntValue("BinningY", value)
 
     def startGrabbing(self):
         # 开始取流
@@ -107,28 +316,25 @@ class MvSdk(CameraSdkInterface):
         # 销毁句柄
         self.destroyHandle()
 
+    def getFrame(self):
+        stOutFrame = MV_FRAME_OUT_INFO_EX()
+        pData = (c_ubyte * self.width * self.height)()  # 假设最大分辨率为2048x2048
+        ret = self.cam.MV_CC_GetOneFrameTimeout(byref(pData), sizeof(pData), stOutFrame, 1000)
+        if ret != 0:
+            raise Exception("采集图像失败")
+        # 数据处理，这里仅将其转换为NumPy数组并显示
+        frame_data = np.frombuffer(pData, count=int(stOutFrame.nWidth * stOutFrame.nHeight), dtype=np.uint8)
+        frame_data = frame_data.reshape((stOutFrame.nHeight, stOutFrame.nWidth))
+        if ret != 0:
+            raise Exception("采集图像失败")
 
-def main():
-    # 开始取流
-    cam = MvSdk()
-    cam.init()
-    cam.open()
-    cam.startGrabbing()
-    stOutFrame = MV_FRAME_OUT_INFO_EX()
-    pData = (c_ubyte * 3072 * 2048)()  # 假设最大分辨率为2048x2048
-    while True:
-        print(pData)
-        ret = cam.getOneFrameTimeout(pData, stOutFrame, 1000)
-        print(ret)
-        if ret == 0:
-            print(f"成功采集一帧：宽度={stOutFrame.nWidth}，高度={stOutFrame.nHeight}")
-            # 数据处理，这里仅将其转换为NumPy数组并显示
-            frame_data = np.frombuffer(pData, count=int(stOutFrame.nWidth * stOutFrame.nHeight), dtype=np.uint8)
-            frame_data = frame_data.reshape((stOutFrame.nHeight, stOutFrame.nWidth))
-            cv2.imshow("Frame", frame_data)
-            cv2.waitKey(1)
-
+        return frame_data
 
 
 if __name__ == '__main__':
-    main()
+    cam = MvSdk()
+    cam.open()
+    while True:
+        frame = cam.getFrame()
+        cv2.imshow("frame", frame)
+        cv2.waitKey(1)
