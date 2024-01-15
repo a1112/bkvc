26a0f556a90407d7cb8728de5c6b6dea31f036a1
LCX <1042815796@qq.com>
2024-01-16 00:16:02 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='BKVisionCamera',
-    version='0.0.2',
-    description='BKVisionCamera',
-    author='BKVision',
-    author_email='',
-    license="MIT Licence",
-    packages=find_packages(exclude=('BKVisionCamera', 'utils')),
-    platforms="any",
-    package_data={
-    },
-    install_requires=[],
-    zip_safe=False
-)
+
+setup()
